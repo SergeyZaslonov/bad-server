4e21ff348ab22fbbbe1598cf2a5f80535b3f066a
SergeyZaslonov <159817406+SergeyZaslonov@users.noreply.github.com>
2025-11-02 14:55:30 +0300

    customers

--- backend/src/controllers/customers.ts
+++ backend/src/controllers/customers.ts
@@ -15,7 +15,6 @@ export const getCustomers = async (
     try {
         const {
             page = 1,
-            limit = 10,
             sortField = 'createdAt',
             sortOrder = 'desc',
             registrationDateFrom,
@@ -29,6 +28,7 @@ export const getCustomers = async (
             search,
         } = req.query
 
+        const limit = Math.min(Number(req.query.limit) || 10, 10)
         const filters: FilterQuery<Partial<IUser>> = {}
 
         if (registrationDateFrom) {
@@ -116,8 +116,8 @@ export const getCustomers = async (
 
         const options = {
             sort,
-            skip: (Number(page) - 1) * Number(limit),
-            limit: Number(limit),
+            skip: (Number(page) - 1) * limit,
+            limit,
         }
 
         const users = await User.find(filters, null, options).populate([
@@ -137,7 +137,7 @@ export const getCustomers = async (
         ])
 
         const totalUsers = await User.countDocuments(filters)
-        const totalPages = Math.ceil(totalUsers / Number(limit))
+        const totalPages = Math.ceil(totalUsers / limit)
 
         res.status(200).json({
             customers: users,
@@ -145,7 +145,7 @@ export const getCustomers = async (
                 totalUsers,
                 totalPages,
                 currentPage: Number(page),
-                pageSize: Number(limit),
+                pageSize: limit,
             },
         })
     } catch (error) {
